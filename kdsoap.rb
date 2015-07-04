a796488de6799fa1975a185853ff7d8cc5210094
Allen Winter <allen.winter@kdab.com>
2015-07-04 09:35:19 -0400

    kdsoap.rb - homebrew formula for KDSoap

---
+++ kdsoap.rb
@@ -0,0 +1,22 @@
+class Kdsoap < Formula
+  desc "A Qt-based client-side and server-side SOAP component"
+  homepage "https://github.com/KDAB/KDSoap"
+  url "https://build.opensuse.org/source/isv:KDAB/KDSoap/kdsoap-1.4.0.tar.gz"
+  sha1 "36a8075c23f75d9ec4e3e4f58e4128f9bda06dd6"
+
+  depends_on "qt"
+
+  def install
+    touch ".license.accepted"
+    system "./configure.sh", "-shared",
+                          "-release",
+                          "-qmake", "/usr/local/bin/qmake",
+                          "-prefix", "#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "make", "test"
+  end
+end
