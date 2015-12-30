5594c9cf049bba68db766412070a51826b82454a
Filipe Azevedo <filipe.azevedo@kdab.com>
2015-12-30 09:40:20 +0100

    kdstatemachineeditor-qt5 1.0.0

--- kdstatemachineeditor-qt5.rb
+++ kdstatemachineeditor-qt5.rb
@@ -4,7 +4,7 @@ class KdstatemachineeditorQt5 < Formula
   url "https://github.com/KDAB/KDStateMachineEditor/archive/v1.0.0.tar.gz"
   sha256 "ba60e9407c4d21aa8b2ae061d6281f9ded80ab806b5003b1a49367de24034d7f"
 
-  depends_on "qt5"
+  depends_on "qt5" => "with-d-bus"
   depends_on "graphviz"
   depends_on "cmake" => :build
 
