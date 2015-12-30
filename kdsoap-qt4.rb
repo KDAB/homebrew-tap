9d8e3d2ff414ca6355d117f2ce1a1c0142e86330
Filipe Azevedo <filipe.azevedo@kdab.com>
2015-12-30 09:44:04 +0100

    kdsoap-qt4 1.4.0

--- kdsoap-qt4.rb
+++ kdsoap-qt4.rb
@@ -1,11 +1,11 @@
-class Kdsoap < Formula
+class KdsoapQt4 < Formula
   desc "A Qt-based client-side and server-side SOAP component"
   homepage "https://github.com/KDAB/KDSoap"
   url "https://build.opensuse.org/source/isv:KDAB/KDSoap/kdsoap-1.4.0.tar.gz"
   sha1 "36a8075c23f75d9ec4e3e4f58e4128f9bda06dd6"
   head "https://github.com/KDAB/KDSoap.git"
 
-  depends_on "qt"
+  depends_on "qt" => "with-d-bus"
 
   def install
     touch ".license.accepted"
