14f972c40e64601aba61aea2dd95587512feed28
Filipe Azevedo <filipe.azevedo@kdab.com>
2015-12-30 09:38:54 +0100

    gammaray-qt4 2.4.0

--- gammaray-qt4.rb
+++ gammaray-qt4.rb
@@ -4,7 +4,7 @@ class GammarayQt4 < Formula
   url "https://github.com/KDAB/GammaRay/archive/v2.4.0.tar.gz"
   sha256 "9dd517420b132db556a328f272f83dbb2021e4c0f9ee5d3c23659c039cf48718"
 
-  depends_on "qt"
+  depends_on "qt" => "with-d-bus"
   depends_on "graphviz"
   depends_on "cmake" => :build
 
