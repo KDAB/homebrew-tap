0e79667932ea072cf2147f5025a909004630b890
Filipe Azevedo <filipe.azevedo@kdab.com>
2016-03-24 10:34:11 +0100

    Use public github urls.

--- kdsoap-qt4.rb
+++ kdsoap-qt4.rb
@@ -1,7 +1,7 @@
 class KdsoapQt4 < Formula
   desc "A Qt-based client-side and server-side SOAP component"
   homepage "https://github.com/KDAB/KDSoap"
-  url "https://build.opensuse.org/source/isv:KDAB/KDSoap/kdsoap-1.5.0.tar.gz"
+  url "https://github.com/KDAB/KDSoap/releases/download/kdsoap-1.5.0/kdsoap-1.5.0.tar.gz"
   sha256 "da46eb676d7b05393bddf8c78c0dc8256419d61f34e10c2951d9855f2b78d4e6"
   head "https://github.com/KDAB/KDSoap.git"
 
