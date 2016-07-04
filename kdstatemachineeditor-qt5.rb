bcdb9a18dde53825915d9e825a09a1e3f6216e2d
Filipe Azevedo <pasnox@gmail.com>
2016-07-04 10:16:25 +0200

    Fix kdstatemachine tap

--- kdstatemachineeditor-qt5.rb
+++ kdstatemachineeditor-qt5.rb
@@ -2,7 +2,7 @@ class KdstatemachineeditorQt5 < Formula
   desc "A Qt-based framework for creating Qt State Machine metacode using a graphical user interface"
   homepage "https://github.com/KDAB/KDStateMachineEditor"
   url "https://github.com/KDAB/KDStateMachineEditor/archive/v1.1.0.tar.gz"
-  sha256 3dcbb8925d3fc9e1ec760a486477f30856bd66e0086227dc2af0b0afac973a4a"
+  sha256 "3dcbb8925d3fc9e1ec760a486477f30856bd66e0086227dc2af0b0afac973a4a"
   head "https://github.com/KDAB/KDStateMachineEditor.git"
 
   depends_on "qt5" => "with-d-bus"
