10a2ab945ea6de57790075ea409ea1f6a4967a33
Allen Winter <allen.winter@kdab.com>
2016-07-08 17:59:47 -0400

    gammaray-qt4.rb, gammaray-qt5.rb - update to version 2.5.0

--- gammaray-qt5.rb
+++ gammaray-qt5.rb
@@ -1,8 +1,8 @@
 class GammarayQt5 < Formula
   desc "A Qt-based tool to poke around in a Qt-application and also to manipulate the application to some extent."
   homepage "https://github.com/KDAB/GammaRay"
-  url "https://github.com/KDAB/GammaRay/archive/v2.4.1.tar.gz"
-  sha256 "15cc4d44b7757134eb28cb88b37db9f373ff66374455590fd918bb102f966ff1"
+  url "https://github.com/KDAB/GammaRay/archive/v2.5.0.tar.gz"
+  sha256 "2b9e6c936edfe1e42ef359e64ee98c805d7eaf23b0bb0ee87f51eb991a309aa8"
   head "https://github.com/KDAB/GammaRay.git"
 
   depends_on "qt5" => "with-d-bus"
