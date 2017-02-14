9f40785e96f0f6b5beffbe2831bb74a1a3bc0c32
Allen Winter <allen.winter@kdab.com>
2017-02-14 17:04:12 -0500

    gammaray-qt4.rb, gammaray-qt5.rb - Gammary 2.7.0

--- gammaray-qt4.rb
+++ gammaray-qt4.rb
@@ -1,12 +1,11 @@
 class GammarayQt4 < Formula
   desc "A Qt-based tool to poke around in a Qt-application and also to manipulate the application to some extent."
   homepage "https://github.com/KDAB/GammaRay"
-  url "https://github.com/KDAB/GammaRay/archive/v2.6.0.tar.gz"
-  sha256 "762fc1e61fb141462e72fe048b4a7bbf1063eea6a2209963c8aa1ad7696b0217"
+  url "https://github.com/KDAB/GammaRay/archive/v2.7.0.tar.gz"
+  sha256 "74251d9de4bfa31994431c7a493e5de17d0b90853557a245bf3f7f4e0227fd14"
   head "https://github.com/KDAB/GammaRay.git"
 
   depends_on "qt" => "with-d-bus"
-  depends_on "graphviz"
   depends_on "cmake" => :build
 
   def install
