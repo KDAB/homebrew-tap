ee7894ca86b904746e8ea870f1e31934f677b057
Allen Winter <allen.winter@kdab.com>
2017-05-02 13:00:18 -0400

    kdsoap-qt4.rb, kdsoap-qt5.rb - upgrade to v1.6.0

--- kdsoap-qt4.rb
+++ kdsoap-qt4.rb
@@ -1,8 +1,8 @@
 class KdsoapQt4 < Formula
   desc "A Qt-based client-side and server-side SOAP component"
   homepage "https://github.com/KDAB/KDSoap"
-  url "https://github.com/KDAB/KDSoap/releases/download/kdsoap-1.5.1/kdsoap-1.5.1.tar.gz"
-  sha256 "fc4506a91db59aef33aadba9fa828c59047ac7bfdd13a23fe4ba7b66450d4844"
+  url "https://github.com/KDAB/KDSoap/releases/download/kdsoap-1.6.0/kdsoap-1.6.0.tar.gz"
+  sha256 "d6b6b01348d2e1453f7e12724d1848ee41c86a1b19168ca67ac98fedb0408668"
   head "https://github.com/KDAB/KDSoap.git"
 
   depends_on "qt" => "with-d-bus"
