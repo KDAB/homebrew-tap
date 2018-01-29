0b3cce5f1be8776d51614fa8a9b9a3c5da2a875f
Allen Winter <allen.winter@kdab.com>
2018-01-29 15:48:30 -0500

    kdstatemachineeditor-qt5.rb - update for 1.2.2 release

--- kdstatemachineeditor-qt5.rb
+++ kdstatemachineeditor-qt5.rb
@@ -1,8 +1,8 @@
 class KdstatemachineeditorQt5 < Formula
   desc "A Qt-based framework for creating Qt State Machine metacode using a graphical user interface"
   homepage "https://github.com/KDAB/KDStateMachineEditor"
-  url "https://github.com/KDAB/KDStateMachineEditor/archive/v1.2.1.tar.gz"
-  sha256 "a97597aa3c67356bba7f6c1503018ddf3369ada5534f33129adab29a53f6ed11"
+  url "https://github.com/KDAB/KDStateMachineEditor/archive/v1.2.2.tar.gz"
+  sha256 "d616acc9cea6dc6ad4731d3a2fe12e19cf9ce6d3a59b4cd685cf719bd1b82637"
   head "https://github.com/KDAB/KDStateMachineEditor.git"
 
   depends_on "qt5" => "with-d-bus"
