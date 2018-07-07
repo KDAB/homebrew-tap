6a3c54c0fe80b95aa5946dfaf30b6acff9733889
Allen Winter <allen.winter@kdab.com>
2018-07-07 17:12:54 -0400

    charm-qt5.rb - update to 1.12.0rc2

--- charm-qt5.rb
+++ charm-qt5.rb
@@ -1,15 +1,15 @@
 class CharmQt5 < Formula
   desc "A Qt-based Cross-Platform Time Tracker"
   homepage "https://github.com/KDAB/Charm"
-  url "https://github.com/KDAB/Charm/archive/1.11.4.tar.gz"
-  sha256 "24f31c76e30a56e87647a8aafa87e448e86e23e9e796c225b0e824a40dfb663b"
+  url "https://github.com/KDAB/Charm/archive/1.12.0rc2.tar.gz"
+  sha256 "7c6879bc474d33d66675db9a23e9661ba0f329cd3bf9a23ce45d565383730deb"
   head "https://github.com/KDAB/Charm.git"
 
   depends_on "qt5" => "with-d-bus"
   depends_on "cmake" => :build
 
   def install
-    system "cmake", ".", "-DCharm_VERSION=1.10.0", *std_cmake_args
+    system "cmake", ".", "-DCharm_VERSION=1.12.0rc2", *std_cmake_args
     system "make"
     system "make", "install"
   end
