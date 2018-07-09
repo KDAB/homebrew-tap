d95c1d9701add60ecafb1c7c6a56f5d2dcb9dd89
Allen Winter <allen.winter@kdab.com>
2018-07-09 11:44:16 -0400

    charm-qt5.rb - update to v1.12.0 final

--- charm-qt5.rb
+++ charm-qt5.rb
@@ -1,8 +1,8 @@
 class CharmQt5 < Formula
   desc "A Qt-based Cross-Platform Time Tracker"
   homepage "https://github.com/KDAB/Charm"
-  url "https://github.com/KDAB/Charm/archive/1.12.0rc2.tar.gz"
-  sha256 "7c6879bc474d33d66675db9a23e9661ba0f329cd3bf9a23ce45d565383730deb"
+  url "https://github.com/KDAB/Charm/archive/1.12.0.tar.gz"
+  sha256 "82e1b2356bd5a29f4b6b19b6c7dc7a8b0ce32574a5999b617647743e012f52b6"
   head "https://github.com/KDAB/Charm.git"
 
   depends_on "qt5" => "with-d-bus"
