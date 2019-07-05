e3e1f278f513d20bc4555d70738d7f50f050dca8
Allen Winter <allen.winter@kdab.com>
2019-07-05 13:07:26 -0400

    gammaray*.rb - update GammaRay for the v2.11.0 release
    note that support for Qt4 has been removed

--- gammaray-qt5.rb
+++ gammaray-qt5.rb
@@ -1,8 +1,8 @@
 class GammarayQt5 < Formula
   desc "A Qt-based tool to poke around in a Qt-application and also to manipulate the application to some extent."
   homepage "https://github.com/KDAB/GammaRay"
-  url "https://github.com/KDAB/GammaRay/archive/v2.10.0.tar.gz"
-  sha256 "d9a816eb8f1ac1ae227d280d84ef23aee83e99fadc5269ef53d53d0aad5496d2"
+  url "https://github.com/KDAB/GammaRay/archive/v2.11.0.tar.gz"
+  sha256 "2f1bcb78b8a2c5715414fe9ab9eba0128dedbcdb7ee6dddd6e559bae168978a9"
   head "https://github.com/KDAB/GammaRay.git"
 
   depends_on "qt5" => "with-d-bus"
