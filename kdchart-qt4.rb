1e09df7094473175566d8ba23b7bcea36ba347dd
Allen Winter <allen.winter@kdab.com>
2020-01-08 18:09:33 -0500

    kdchart-qt4.rb, kdchart-qt5.rb - release 2.7.0

--- kdchart-qt4.rb
+++ kdchart-qt4.rb
@@ -1,8 +1,8 @@
-class KdchartQt4 < Formula
+wclass KdchartQt4 < Formula
   desc "A Qt library for creating business and scientific charts"
   homepage "https://github.com/KDAB/KDChart"
-  url "https://github.com/KDAB/KDChart/releases/download/kdchart-2.6.2/kdchart-2.6.2.tar.gz"
-  sha256 "e3f0f2915f68702d7ea2c45af9b59a39e6a5181290817efbf760d4adba0b2f01"
+  url "https://github.com/KDAB/KDChart/releases/download/kdchart-2.7.0/kdchart-2.7.0.tar.gz"
+  sha256 "3c4e37b75eb9f1f79d2920bc2b338594aebf0c0715943816262f580f0cabad79"
   head "https://github.com/KDAB/KDChart.git"
 
   depends_on "qt" => "with-d-bus"
