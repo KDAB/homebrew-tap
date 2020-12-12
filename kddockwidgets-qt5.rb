81be0760b314f3ee94dd5fdc83fbb871f974d56d
Allen Winter <allen.winter@kdab.com>
2020-12-12 08:06:43 -0500

    kddockwidgets-qt.rb - follow 1.1.1 release

--- kddockwidgets-qt5.rb
+++ kddockwidgets-qt5.rb
@@ -1,8 +1,8 @@
 class KddockwidgetsQt5 < Formula
   desc "A dock widget library implementing advanced functionalities missing in Qt"
   homepage "https://github.com/KDAB/KDDockWidgets"
-  url "https://github.com/KDAB/KDDockWidgets/releases/download/v1.1.0/kddockwidgets-1.1.0.tar.gz"
-  sha256 "f1349a34ced2ec5c8daec25fe09cb8593a5a3cb55bea6542b8f6c1f4e863d9a4"
+  url "https://github.com/KDAB/KDDockWidgets/releases/download/v1.1.1/kddockwidgets-1.1.1.tar.gz"
+  sha256 "2aa0d4d2f7d041d43de14118f5f1ef65111a78c90e570b8fd173c3ada90adc3f"
   head "https://github.com/KDAB/KDDockWidgets.git"
 
   depends_on "qt5" => "with-d-bus"
