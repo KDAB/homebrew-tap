0a30feddfea95599575259dc89f94993bc34969b
Allen Winter <allen.winter@kdab.com>
2020-12-22 12:27:50 -0500

    kdsoap-qt5.rb - follow 1.10.0 release

--- kdsoap-qt5.rb
+++ kdsoap-qt5.rb
@@ -1,8 +1,8 @@
 class KdsoapQt5 < Formula
   desc "A Qt-based client-side and server-side SOAP component"
   homepage "https://github.com/KDAB/KDSoap"
-  url "https://github.com/KDAB/KDSoap/releases/download/kdsoap-1.9.0/kdsoap-1.9.1.tar.gz"
-  sha256 "a020ea26e91a2bcdbfa7bc631870ed07be2c583ae29114cfe72a5a94e0e93d27"
+  url "https://github.com/KDAB/KDSoap/releases/download/kdsoap-1.10.0/kdsoap-1.10.0.tar.gz"
+  sha256 "0c606ecc434466dbadb2829820ca163af60cc7ca226f553b5d4032196737338f"
   head "https://github.com/KDAB/KDSoap.git"
 
   depends_on "qt5" => "with-d-bus"
