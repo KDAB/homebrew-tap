5a6a8f469aa509321c64007b5c5fff3ebaca666d
Allen Winter <allen.winter@kdab.com>
2021-06-30 18:25:19 -0400

    kdsoap-qt5.rb, kdsoap-qt6.rb - follow 2.0.0 release

--- kdsoap-qt6.rb
+++ kdsoap-qt6.rb
@@ -1,15 +1,15 @@
-class KdsoapQt5 < Formula
-  desc "A Qt5-based client-side and server-side SOAP component"
+class KdsoapQt6 < Formula
+  desc "A Qt6-based client-side and server-side SOAP component"
   homepage "https://github.com/KDAB/KDSoap"
   url "https://github.com/KDAB/KDSoap/releases/download/kdsoap-2.0.0/kdsoap-2.0.0.tar.gz"
   sha256 "d18963104fa6f7d02b044631cddbe78f18f70e06c607af680c7ace04d6cf04ee"
   head "https://github.com/KDAB/KDSoap.git"
 
-  depends_on "qt5" => "with-d-bus"
+  depends_on "qt6" => "with-d-bus"
   depends_on "cmake" => :build
 
   def install
-    system "cmake", ".", *std_cmake_args
+    system "cmake", ".", "-DKDSoap_QT6=True", *std_cmake_args
     system "make"
     system "make", "install"
   end
