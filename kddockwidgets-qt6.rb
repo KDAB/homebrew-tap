ca2ba4d19ef8b7a97cbba9edbd156e778811adb5
Allen Winter <allen.winter@kdab.com>
2021-07-16 12:42:05 -0400

    kddockwidgets-qt5, kddockwidgets-qt6 - follow 1.4.0 release

--- kddockwidgets-qt6.rb
+++ kddockwidgets-qt6.rb
@@ -1,8 +1,8 @@
 class KddockwidgetsQt6 < Formula
   desc "Advanced Dock Widget Framework for Qt6"
   homepage "https://github.com/KDAB/KDDockWidgets"
-  url "https://github.com/KDAB/KDDockWidgets/releases/download/v1.3.1/kddockwidgets-1.3.1.tar.gz"
-  sha256 "5b1a532590ce4bf01147450f946d7460754d0424487ce4eb4f563d1b8e8c4449"
+  url "https://github.com/KDAB/KDDockWidgets/releases/download/v1.4.0/kddockwidgets-1.4.0.tar.gz"
+  sha256 "e8cc52ee49c6aa8fe4caae3d3a58370fc6dda9d7936621c46a514a9726b5177d"
   head "https://github.com/KDAB/KDDockWidgets.git"
 
   depends_on "qt6" => "with-d-bus"
