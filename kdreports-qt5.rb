a17cb2a4b99fd77812d0084d353a63007aa90046
Allen Winter <allen.winter@kdab.com>
2022-01-12 06:39:12 -0500

    kdreports-qt[5,6].rb - follow 2.1.0 release

--- kdreports-qt5.rb
+++ kdreports-qt5.rb
@@ -1,8 +1,8 @@
 class KdreportsQt5 < Formula
   desc "A Qt5-based library for creating printable reports"
   homepage "https://github.com/KDAB/KDReports"
-  url "https://github.com/KDAB/KDReports/releases/download/kdreports-2.0.0/kdreports-2.0.0.tar.gz"
-  sha256 "24f68fc93d7565eb7077fbfc9cff2b37fdf44611d5b281e6ef0aa3a9636b78b4"
+  url "https://github.com/KDAB/KDReports/releases/download/kdreports-2.1.0/kdreports-2.1.0.tar.gz"
+  sha256 "ec2f847fbecf2334da44d84b06ec8986ecd7a63f213c2e8bb1141ae525a4786f"
   head "https://github.com/KDAB/KDReports.git"
 
   depends_on "qt5" => "with-d-bus"
