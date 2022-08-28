0d407333fbf8b9ddfac200a8a7897996df54107f
Allen Winter <allen.winter@kdab.com>
2022-08-28 11:04:34 -0400

    kdchart-qt[5,6].rb - follow 3.0.0 release

--- kdchart-qt6.rb
+++ kdchart-qt6.rb
@@ -1,15 +1,15 @@
-class KdchartQt5 < Formula
-  desc "A Qt5-based library for creating business and scientific charts"
+class KdchartQt6 < Formula
+  desc "A Qt6-based library for creating business and scientific charts"
   homepage "https://github.com/KDAB/KDChart"
   url "https://github.com/KDAB/KDChart/releases/download/kdchart-3.0.0/kdchart-3.0.0.tar.gz"
   sha256 "57359cb9593b5a7866b4b41d9397ca4386316559d449a57b4fe2ab491bb304e9"
   head "https://github.com/KDAB/KDChart.git"
 
-  depends_on "qt5" => "with-d-bus"
+  depends_on "qt6" => "with-d-bus"
   depends_on "cmake" => :build
 
   def install
-    system "cmake", ".", *std_cmake_args
+    system "cmake", ".", "-DKDChart_QT6=True", *std_cmake_args
     system "make"
     system "make", "install"
   end
