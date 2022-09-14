9a0a373c531da1d8f7bd98fe4ede733b728c34ce
Allen Winter <allen.winter@kdab.com>
2022-09-14 09:45:24 -0400

    kdsoap-[qt5,qt6].rb - follow v2.1.1 release

--- kdsoap-qt6.rb
+++ kdsoap-qt6.rb
@@ -1,8 +1,8 @@
 class KdsoapQt6 < Formula
   desc "A Qt6-based client-side and server-side SOAP component"
   homepage "https://github.com/KDAB/KDSoap"
-  url "https://github.com/KDAB/KDSoap/releases/download/kdsoap-2.1.0/kdsoap-2.1.0.tar.gz"
-  sha256 "3eec5568a14ff575b3d14c582de62dd8fddd132429859a6d8cea9892709f5ff4"
+  url "https://github.com/KDAB/KDSoap/releases/download/kdsoap-2.1.1/kdsoap-2.1.1.tar.gz"
+  sha256 "aed57f6b200ddf762f5d2898f7e9228dd0700881c4491aefe4006f7fa5f5c627"
   head "https://github.com/KDAB/KDSoap.git"
 
   depends_on "qt6" => "with-d-bus"
