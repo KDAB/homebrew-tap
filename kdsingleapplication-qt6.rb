6e39779c36b6fa3c18a819ace881256c7eb0d439
Allen Winter <allen.winter@kdab.com>
2023-07-21 16:24:35 -0400

    kdsingleapplication-[qt5,qt6].rb - add KDSingleApplication
    first release 1.0.0

---
+++ kdsingleapplication-qt6.rb
@@ -0,0 +1,20 @@
+class KdsingleapplicationQt6 < Formula
+  desc "Helper class for Qt6 single-instance policy applications"
+  homepage "https://github.com/KDAB/KDSingleApplication"
+  url "https://github.com/KDAB/KDSingleApplication/releases/download/v1.0.0/kdsingleapplication-1.0.0.tar.gz"
+  sha256 "c92355dc10f3ebd39363458458fb5bdd9662e080cf77d91f0437763c4d936520"
+  head "https://github.com/KDAB/KDSingleApplication.git"
+
+  depends_on "qt6" => "with-d-bus"
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", ".", "-DKDSingleApplication_QT6=True", *std_cmake_args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "make", "test"
+  end
+end
