5501a971a13000de1d469ed909da54da886cc712
Allen Winter <allen.winter@kdab.com>
2023-12-22 10:31:03 -0500

    kdsingleapplication-qt[5,6].rb - follow 1.1.0 release

--- kdsingleapplication-qt5.rb
+++ kdsingleapplication-qt5.rb
@@ -1,8 +1,8 @@
 class KdsingleapplicationQt5 < Formula
   desc "Helper class for Qt5 single-instance policy applications"
   homepage "https://github.com/KDAB/KDSingleApplication"
-  url "https://github.com/KDAB/KDSingleApplication/releases/download/v1.0.0/kdsingleapplication-1.0.0.tar.gz"
-  sha256 "c92355dc10f3ebd39363458458fb5bdd9662e080cf77d91f0437763c4d936520"
+  url "https://github.com/KDAB/KDSingleApplication/releases/download/v1.1.0/kdsingleapplication-1.1.0.tar.gz"
+  sha256 "31029fffa4873e2769c555668e8edaa6bd5721edbc445bff5e66cc6af3b9ed78"
   head "https://github.com/KDAB/KDSingleApplication.git"
 
   depends_on "qt5" => "with-d-bus"
