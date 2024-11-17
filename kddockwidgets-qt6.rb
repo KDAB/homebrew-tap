5964974f7183230d7222f39162ee700e0657f98b
Sergio Martins <sergio.martins@kdab.com>
2024-11-17 02:58:20 +0000

    Bump KDDW to 2.1

--- kddockwidgets-qt6.rb
+++ kddockwidgets-qt6.rb
@@ -1,15 +1,15 @@
 class KddockwidgetsQt6 < Formula
   desc "Advanced Dock Widget Framework for Qt6"
   homepage "https://github.com/KDAB/KDDockWidgets"
-  url "https://github.com/KDAB/KDDockWidgets/releases/download/v2.0.0/kddockwidgets-2.0.0.tar.gz"
-  sha256 "10514846c8111812d575eaec701c7fef2d3a169d4ef2256fa50f52f70f508598"
+  url "https://github.com/KDAB/KDDockWidgets/releases/download/v2.1.0/kddockwidgets-2.1.0.tar.gz"
+  sha256 "cf3242b8fde8988b2661366b6a9597bcb67164074c4f31d03ec2999b475a25d7"
   head "https://github.com/KDAB/KDDockWidgets.git"
 
   depends_on "qt6" => "with-d-bus"
   depends_on "cmake" => :build
 
   def install
-    system "cmake", ".", "-DKDDockWidgets_QT6=True", *std_cmake_args
+    system "cmake", ".", "-DKDDockWidgets_NO_SPDLOG=ON", "-DKDDockWidgets_QT6=True", *std_cmake_args
     system "make"
     system "make", "install"
   end
