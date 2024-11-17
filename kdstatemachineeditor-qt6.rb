f7c3a2cf7f70bae064ffad92d8fa3b85e23de0ba
Sergio Martins <sergio.martins@kdab.com>
2024-11-17 12:41:34 +0000

    Update KDSME to latest master
    Anything before 2.0 doesn't build anymore

--- kdstatemachineeditor-qt6.rb
+++ kdstatemachineeditor-qt6.rb
@@ -1,4 +1,4 @@
-class KdstatemachineeditorQt5 < Formula
+class KdstatemachineeditorQt6 < Formula
   desc "A Qt-based framework for creating Qt State Machine metacode using a graphical user interface"
   homepage "https://github.com/KDAB/KDStateMachineEditor"
   url "https://github.com/KDAB/KDStateMachineEditor/archive/4ac468cb3576b44a74c69567120f043613b1a8a9.tar.gz"
@@ -6,7 +6,7 @@ class KdstatemachineeditorQt5 < Formula
   version "2.0.0-beta1"
   head "https://github.com/KDAB/KDStateMachineEditor.git"
 
-  depends_on "qt5" => "with-d-bus"
+  depends_on "qt6" => "with-d-bus"
   depends_on "cmake" => :build
   depends_on "bison" => :build
   depends_on "graphviz"
@@ -15,7 +15,7 @@ class KdstatemachineeditorQt5 < Formula
     bison_bin = "#{HOMEBREW_PREFIX}/opt/bison/bin"
     ENV["PATH"] = "#{bison_bin}:#{ENV["PATH"]}"
 
-    system "cmake", "-DWITH_INTERNAL_GRAPHVIZ=OFF", ".", *std_cmake_args
+    system "cmake", "-DBUILD_QT6=ON", "-DWITH_INTERNAL_GRAPHVIZ=OFF", ".", *std_cmake_args
     system "make"
     system "make", "install"
   end
