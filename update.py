9d273148a2577c126ad0875f5c31c77a1a4f391b
Sergio Martins <sergio.martins@kdab.com>
2024-11-18 00:00:40 +0000

    Add info.py
    Prints outdated packages

--- update.py
+++ update.py
@@ -12,33 +12,7 @@
 
 import argparse, os
 import hashlib
-
-def exit_because(reason):
-    print(reason)
-    exit(1)
-
-def run_command(command, fatal = True):
-    if os.system(command) == 0:
-        return True;
-
-    if fatal:
-        exit_because(f"Failed to run command: {command}")
-
-    return False
-
-# Returns the url of the tar.gz
-def get_url(filename) -> str:
-    try:
-        with open(filename, 'r') as f:
-            for line in f:
-                if 'url' in line.lower():
-                    url = line.split('"')[1]  # Extract URL between quotes
-                    return url
-    except Exception as e:
-        exit_because(f"Error reading file: {e}")
-
-    exit_because("Could not find url in file")
-    return ""
+from utils import *
 
 # Returns the sha256 of the .tar.gz file
 def get_tarball_sha256(url):
