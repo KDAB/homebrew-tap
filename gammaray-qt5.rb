f0e82ba5f69b895a6cb837c7f04ed2190333d0a6
Anka <runner@Mac-1731963310499.local>
2024-11-18 21:15:41 +0000

    Update package gammaray-qt5 to v3.1.0

--- gammaray-qt5.rb
+++ gammaray-qt5.rb
@@ -1,8 +1,8 @@
 class GammarayQt5 < Formula
   desc "A Qt-based tool to poke around in a Qt-application and also to manipulate the application to some extent."
   homepage "https://github.com/KDAB/GammaRay"
-  url "https://github.com/KDAB/GammaRay/releases/download/v2.11.3/gammaray-2.11.3.tar.gz"
-  sha256 "03d7ca7bd5eb600c9c389d0cf071960330592f1f392a783b7fec5f9eaa5df586"
+  url "https://github.com/KDAB/GammaRay/releases/download/v3.1.0/gammaray-3.1.0.tar.gz"
+  sha256 "93b52d5318374896621e1d8b5dd03379c53e0458b1633b539d18737fe8c300cf"
   head "https://github.com/KDAB/GammaRay.git"
 
   depends_on "qt@5" => "with-d-bus"
