55f11516431e891a1b8827954052fb4069cc9bcc
Anka <runner@Mac-1732240749667.local>
2024-11-22 03:17:36 +0000

    Update package kdalgorithms to 1.3

--- kdalgorithms.rb
+++ kdalgorithms.rb
@@ -1,8 +1,8 @@
 class Kdalgorithms < Formula
   desc "Algorithm helpers - kdalgorithms"
   homepage "https://github.com/KDAB/KDAlgorithms"
-  url "https://github.com/KDAB/KDAlgorithms/archive/357844a5936186cf9042fed0ced08db1cdb92942.tar.gz"
-  sha256 "6734530b84676d402c85cf31f52631c8787037c59b9b2e515e4afe94cbe51e26"
+  url "https://github.com/KDAB/KDAlgorithms/archive/1.3.tar.gz"
+  sha256 "3bf02aebec20f23d3c1389c3fb15bc54874814bb34fd399314e28ddf7bc26598"
   head "https://github.com/KDAB/KDAlgorithms.git"
   version "1.3.0-beta1"
 
