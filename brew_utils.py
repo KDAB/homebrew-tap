44eac13456b2274943dff60a7a6a3c5d7ffdd260
Sergio Martins <sergio.martins@kdab.com>
2024-12-01 22:29:13 +0000

    Remove hardcoded config, port to releasing.toml
    Now we read the ci_release_tools/releasing.toml, which contains
    which repos we generate formulas for

--- brew_utils.py
+++ brew_utils.py
@@ -1,43 +1,40 @@
 #!/usr/bin/env python3
 # SPDX-License-Identifier: MIT
 
-import sys, os
+import sys, os, tomllib
+from pathlib import Path
 
+sys.path.append(str(Path(__file__).parent / "ci_release_tools" / "src"))
 from ci_release_tools.src.utils import *
 from ci_release_tools.src.gh_utils import *
 from brew_utils import *
 
-formulas = {
-    'kddockwidgets-qt5.rb': {"repo": "KDAB/KDDockWidgets"},
-    'kddockwidgets-qt6.rb': {"repo": "KDAB/KDDockWidgets"},
-    'kdsingleapplication-qt5.rb': {"repo": "KDAB/KDSingleApplication"},
-    'kdsingleapplication-qt6.rb': {"repo": "KDAB/KDSingleApplication"},
-    'kdsoap-qt5.rb': {"repo": "KDAB/KDSoap"},
-    'kdsoap-qt6.rb': {"repo": "KDAB/KDSoap"},
-    'kdchart-qt5.rb': {"repo": "KDAB/KDChart"},
-    'kdchart-qt6.rb': {"repo": "KDAB/KDChart"},
-    'kdreports-qt5.rb': {"repo": "KDAB/KDReports"},
-    'kdreports-qt6.rb': {"repo": "KDAB/KDReports"},
-    'gammaray-qt5.rb': {"repo": "KDAB/Gammaray"},
-    'kdmactouchbar-qt5.rb': {"repo": "KDAB/KDMacTouchBar"},
-    'kdbindings.rb': {"repo": "KDAB/KDBindings"},
-    'kdutils.rb': {"repo": "KDAB/KDUtils"},
-    # 'kdalgorithms.rb': {"repo": "KDAB/KDAlgorithms"},
-    # 'kdstatemachineeditor-qt5.rb': {"repo": "KDAB/KDStateMachineEditor"},
-    # 'kdstatemachineeditor-qt6.rb': {"repo": "KDAB/KDStateMachineEditor"},
-}
 
-def unique_repos():
-    seen_repos = set()
-    unique_repo_list = []
+def get_projects():
+    with open('ci_release_tools/releasing.toml', 'rb') as f:
+        global formulas
+        toml_content = tomllib.load(f)
+        return toml_content['project']
 
-    for filename in formulas:
-        repo = formulas[filename]["repo"]
-        if repo not in seen_repos:
-            unique_repo_list.append(repo)
-            seen_repos.add(repo)
 
-    return unique_repo_list
+# Returns a list like; ['KDAB/KDSoap', 'KDAB/KDReports']
+def repos():
+    repo_list = []
+    projects = get_projects()
+    for p in projects:
+        if 'homebrew' in projects[p] and projects[p]['homebrew']:
+            repo_list.append(f"KDAB/{p}")
+
+    return repo_list
+
+# Returns the list of .rb filenames
+def get_formula_filenames():
+    filenames = []
+    projects = get_projects()
+    for p in projects:
+        if 'homebrew' in projects[p] and projects[p]['homebrew']:
+            filenames.extend(projects[p]['homebrew'])
+    return filenames
 
 # Gets the package version from an .rb file
 def get_version_in_brew(filename):
@@ -72,14 +69,21 @@ def get_url(filename) -> str:
 # [('kdmactouchbar-qt5.rb', 'v1.0.0', 'v1.1.3')]
 def get_outdated_packages():
     packages = []
-    for filename in formulas:
-        current_version = get_version_in_brew(filename)
-        repo = formulas[filename]["repo"]
-        github_version = get_latest_release_tag_in_github(repo)
-        github_version_numeric = clean_version(github_version)
+    projects = get_projects()
 
-        # print(f"testing {repo} {current_version} {github_version_numeric}")
-        if current_version != github_version_numeric:
-            packages.append((filename, current_version, github_version))
+    for project_name, project_data in projects.items():
+        if 'homebrew' not in project_data:
+            continue
+
+        filenames = project_data['homebrew']
+        for filename in filenames:
+            current_version = get_version_in_brew(filename)
+            repo = f"KDAB/{project_name}"
+            github_version = get_latest_release_tag_in_github(repo)
+            github_version_numeric = clean_version(github_version)
+
+            # print(f"testing {repo} {current_version} {github_version_numeric}")
+            if current_version != github_version_numeric:
+                packages.append((filename, current_version, github_version))
 
     return packages
