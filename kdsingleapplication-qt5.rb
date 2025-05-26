a4b39d9195d9cac67c6e4ec54bb9704c0182dc1c
Anka <runner@Mac-1748272369356.local>
2025-05-26 15:41:36 +0000

    Update package kdsingleapplication-qt5 to v1.2.0

--- kdsingleapplication-qt5.rb
+++ kdsingleapplication-qt5.rb
@@ -1,8 +1,8 @@
 class KdsingleapplicationQt5 < Formula
   desc "Helper class for Qt5 single-instance policy applications"
   homepage "https://github.com/KDAB/KDSingleApplication"
-  url "https://github.com/KDAB/KDSingleApplication/releases/download/v1.1.0/kdsingleapplication-1.1.0.tar.gz"
-  sha256 "31029fffa4873e2769c555668e8edaa6bd5721edbc445bff5e66cc6af3b9ed78"
+  url "https://github.com/KDAB/KDSingleApplication/releases/download/v1.2.0/kdsingleapplication-1.2.0.tar.gz"
+  sha256 "ff4ae6a4620beed1cdb3e6a9b78a17d7d1dae7139c3d4746d4856b7547d42c38"
   head "https://github.com/KDAB/KDSingleApplication.git"
 
   depends_on "qt@5" => "with-d-bus"
