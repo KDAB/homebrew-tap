331ffe8de7444c8e7dd9e2bdf2de76f35a81067b
Anka <runner@iad20-eo1208-1ca74280-f7c1-4153-a807-93f7a2cccd24-76EA37458C00.local>
2025-10-01 21:10:24 +0000

    Update package gammaray-qt6 to v3.3.0

--- gammaray-qt6.rb
+++ gammaray-qt6.rb
@@ -1,8 +1,8 @@
 class GammarayQt6 < Formula
   desc "A Qt-based tool to poke around in a Qt-application and also to manipulate the application to some extent."
   homepage "https://github.com/KDAB/GammaRay"
-  url "https://github.com/KDAB/GammaRay/releases/download/v3.2.2/gammaray-3.2.2.tar.gz"
-  sha256 "18830a83ca8ba8e6e07d78a88d933c2666eda4b26d3cbdc97e06914f5e92388c"
+  url "https://github.com/KDAB/GammaRay/releases/download/v3.3.0/gammaray-3.3.0.tar.gz"
+  sha256 "03fda338025d31b6a2794550f090d538996bbb6f4888eab3acb39db5a17127a8"
   head "https://github.com/KDAB/GammaRay.git"
 
   depends_on "qt@6" => "with-d-bus"
